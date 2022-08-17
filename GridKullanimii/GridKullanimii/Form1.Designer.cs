7e0930260cf15ef8f505ef31f5ae2f349ed37169
Esra Ertsak <ertsakesra@gmail.com>
2022-08-17 12:59:37 +0300

    GridControl Kullanımı

---
+++ GridKullanimii/GridKullanimii/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace GridKullanimii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.urunlerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.urunlerDataSet = new GridKullanimii.UrunlerDataSet();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.UrunAd = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.Fiyat = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.UrunId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtAD = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.urunlerTableAdapter = new GridKullanimii.UrunlerDataSetTableAdapters.UrunlerTableAdapter();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunlerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunlerDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.urunlerBindingSource;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(526, 480);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.Click += new System.EventHandler(this.gridControl1_Click);
+            // 
+            // urunlerBindingSource
+            // 
+            this.urunlerBindingSource.DataMember = "Urunler";
+            this.urunlerBindingSource.DataSource = this.urunlerDataSet;
+            // 
+            // urunlerDataSet
+            // 
+            this.urunlerDataSet.DataSetName = "UrunlerDataSet";
+            this.urunlerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.UrunAd,
+            this.Fiyat,
+            this.UrunId});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.FocusedRowChanged += new DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(this.gridView1_FocusedRowChanged_1);
+            // 
+            // UrunAd
+            // 
+            this.UrunAd.Caption = "Ürün Adı";
+            this.UrunAd.FieldName = "UrunAd";
+            this.UrunAd.Name = "UrunAd";
+            this.UrunAd.Visible = true;
+            this.UrunAd.VisibleIndex = 1;
+            // 
+            // Fiyat
+            // 
+            this.Fiyat.Caption = "Fiyat";
+            this.Fiyat.FieldName = "UrunFiyat";
+            this.Fiyat.Name = "Fiyat";
+            this.Fiyat.Visible = true;
+            this.Fiyat.VisibleIndex = 2;
+            // 
+            // UrunId
+            // 
+            this.UrunId.Caption = "Ürün ID";
+            this.UrunId.FieldName = "UrunId";
+            this.UrunId.Name = "UrunId";
+            this.UrunId.Visible = true;
+            this.UrunId.VisibleIndex = 0;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(637, 92);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(135, 21);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtAD
+            // 
+            this.txtAD.Location = new System.Drawing.Point(637, 132);
+            this.txtAD.Name = "txtAD";
+            this.txtAD.Size = new System.Drawing.Size(135, 21);
+            this.txtAD.TabIndex = 2;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(637, 176);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(135, 21);
+            this.txtFiyat.TabIndex = 3;
+            // 
+            // urunlerTableAdapter
+            // 
+            this.urunlerTableAdapter.ClearBeforeFill = true;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(604, 92);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(18, 19);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "id:\r\n";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(562, 134);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(69, 19);
+            this.labelControl1.TabIndex = 7;
+            this.labelControl1.Text = "Ürün Adı:";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(545, 175);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(86, 19);
+            this.labelControl2.TabIndex = 8;
+            this.labelControl2.Text = "Ürün Fiyatı:";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(537, 229);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(85, 35);
+            this.simpleButton1.TabIndex = 9;
+            this.simpleButton1.Text = "EKLE";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(637, 229);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(94, 37);
+            this.simpleButton2.TabIndex = 10;
+            this.simpleButton2.Text = "SİL";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.simpleButton3.Location = new System.Drawing.Point(748, 229);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(110, 39);
+            this.simpleButton3.TabIndex = 11;
+            this.simpleButton3.Text = "GÜNCELLE";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 488);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtAD);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunlerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunlerDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn UrunId;
+        private DevExpress.XtraGrid.Columns.GridColumn UrunAd;
+        private DevExpress.XtraGrid.Columns.GridColumn Fiyat;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtAD;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private UrunlerDataSet urunlerDataSet;
+        private System.Windows.Forms.BindingSource urunlerBindingSource;
+        private UrunlerDataSetTableAdapters.UrunlerTableAdapter urunlerTableAdapter;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+    }
+}
+
---
+++ GridKullanimii/GridKullanimii/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GridKullanimii
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'urunlerDataSet.Urunler' table. You can move, or remove it, as needed.
+            this.urunlerTableAdapter.Fill(this.urunlerDataSet.Urunler);
+            VerileriGoster();
+
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-ASFN1BE;Initial Catalog=Urunler;Integrated Security=True");
+        private void VerileriGoster()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Urunler", baglanti);
+            DataTable datatable = new DataTable();
+            adapter.Fill(datatable);
+            gridControl1.DataSource = datatable;
+        }
+
+  
+
+        private void gridView1_FocusedRowChanged_1(object sender, DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs e)
+        {
+            DataRow row = gridView1.GetDataRow(gridView1.FocusedRowHandle);
+            if (row != null)
+            {
+                txtID.Text = row[0].ToString();
+                txtAD.Text = row[1].ToString();
+                txtFiyat.Text = row[2].ToString();
+            }
+        }
+        private void VerileriTemizle()
+        {
+            txtID.Text = "";
+            txtAD.Text = "";
+            txtFiyat.Text = "";
+           
+        }
+        //Silme 
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Text);
+
+            if (id != 0)
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("delete from Urunler  where UrunId=@id", baglanti);
+                komut.Parameters.AddWithValue("@id", id);
+                komut.ExecuteNonQuery();
+
+                baglanti.Close();
+                MessageBox.Show(txtAD.Text + " ürünü başarı ile silindi!");
+                VerileriGoster();
+                VerileriTemizle();
+
+            }
+            else
+            {
+                DialogResult dialog = MessageBox.Show("Lütfen silmek için ürün seçin!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //Ekleme
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Urunler WHERE UrunId=", baglanti);
+           
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("INSERT INTO  Urunler (UrunAd,UrunFiyat) VALUES (@ad, @fiyat)", baglanti);
+            
+            komut.Parameters.AddWithValue("@ad", txtAD.Text);
+            komut.Parameters.AddWithValue("@fiyat", txtFiyat.Text);
+            komut.ExecuteNonQuery();
+            MessageBox.Show(txtAD.Text + " ayarı başarılı bir şekilde eklendi!");
+            baglanti.Close();
+            VerileriGoster();
+            VerileriTemizle();
+        }
+        //Güncelleme
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Text);
+            if (txtID.Text != "" && txtAD.Text != "" && txtFiyat.Text != "" )
+            {
+
+                SqlCommand komut = new SqlCommand("UPDATE Urunler set   UrunAd=@ad,UrunFiyat=@fiyat  where UrunId=@id", baglanti);
+                baglanti.Open();
+                komut.Parameters.AddWithValue("@id", txtID.Text);
+                komut.Parameters.AddWithValue("@ad", txtAD.Text);
+                komut.Parameters.AddWithValue("@fiyat", txtFiyat.Text);          
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Başarılı bir şekilde güncellendi!");
+                baglanti.Close();
+                VerileriGoster();
+                VerileriTemizle();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen güncellemek için bir sütun seçin!");
+            }
+        }
+    }
+}
